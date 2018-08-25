7f6c690a292840f1dbc05df2b4e7f05dd030d444
Li Xinyue <lxy_2008focus@126.com>
2018-08-25 15:26:51 +0800

    仅线性回归能耗预测准确度为73.6

--- DM_EnergyPredict.R
+++ DM_EnergyPredict.R
@@ -2,6 +2,7 @@ library(data.table)
 library(forecast)
 library(ggplot2)
 library(car)
+library(gvlma)
 
 #加载天气数据
 load("杭州2016_2017气象数据.rdata")
@@ -13,7 +14,7 @@ load("2018-6-6.RData")#HZNU用电数据
 
 data.regress.raw <- nn[on_ratio>0&total_elec>0& (month(time) == 5 | month(time) == 6),
                        c("build_code","time","temp_diff","on_ratio","set_temp","real_temp","w_temp","w_hum","time_sep","total_elec")]#夏季温和
-scatterplotMatrix(data.regress.raw[,3:10],smoother.args=list(lty=2),plot.points = FALSE,main="ScatterPlot without 0 EC Data")
+scatterplotMatrix(data.regress.raw[,3:10],smoother=list(lty=2),plot.points = FALSE,main="ScatterPlot without 0 EC Data")
 cor(data.regress.raw[, 3:10], use = "complete.obs")
 #相关矩阵如下(含零能耗记录)
 #             temp_diff    on_ratio    set_temp  real_temp      w_temp       w_hum   time_sep  total_elec
@@ -22,9 +23,24 @@ cor(data.regress.raw[, 3:10], use = "complete.obs")
 #             temp_diff   on_ratio   set_temp  real_temp     w_temp       w_hum    time_sep   total_elec
 # total_elec  0.002200785  0.7604641  0.1256533  0.1528087  0.6136918 -0.32046952  0.74128579  1.000000000
 
-regressFit<-lm(total_elec~time_sep+on_ratio+w_temp+w_hum,data=data.regress.raw)
+#####
+#预测模型前处理
+#考虑异常值
+vif(regressFit)
+sqrt(vif(regressFit))
+outlierTest(regressFit)
+hat.plot(regressFit)
+
+cutoff <- 4/(nrow(data.regress.raw) - length(regressFit$coefficients) - 2)
+plot(regressFit, which = 4, cook.levels = cutoff)
+abline(h = cutoff, lty = 2, col = "red")
+
+influencePlot(regressFit)
+
+regressFit<-lm(total_elec~time_sep+I((on_ratio)^0.5)+w_temp+I(w_temp^2)+I(exp(w_hum)^-1)+I((real_temp)^3)+I(real_temp^2)+I(set_temp),data=data.regress.raw)
 #检验过程
 summary(regressFit)
+cat("he\nllo\n")
 confint(regressFit)
 par(mfrow=c(1,1))
 plot(regressFit)
@@ -35,6 +51,14 @@ crPlots(regressFit)
 ncvTest(regressFit)
 spreadLevelPlot(regressFit,id = TRUE)##有异常
 summary(gvlma(regressFit))
+
+hat.plot <- function(fit){
+  p <- length(coefficients(fit))
+  n <- length(fitted(fit))
+  plot(hatvalues(fit), main = "Index Plot of Hat Values")
+  abline(h = c(2, 3) * p/n, col = "red", lty = 2)
+  identify(1:n, hatvalues(fit), names(hatvalues(fit)))
+}
 
 residplot <- function(fit, nbreaks=10) {
   z <- rstudent(fit)
