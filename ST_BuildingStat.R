a44950c2aecc64f38e2f0777074a0d748966cd97
Li Xinyue <lxy_2008focus@126.com>
2019-03-15 11:21:50 +0800

    从总数据集中拆分教学建筑，并进行简单统计

---
+++ ST_BuildingStat.R
@@ -0,0 +1,24 @@
+#本脚本用于建筑编码及功能的对应，包括其他的简单统计
+
+####读取映射文件####
+data.mapping.buildingType<-as.data.table(read.csv(file = "HZNU_建筑功能及编码对应.csv"))
+data.mapping.buildingType<-data.mapping.buildingType[,c("buildingCode","type")]
+
+
+##简单情况
+# 总末端1492台，教学建筑末端709台
+# 总房间数1162间，教学建筑房间数385间
+
+####对于行为/能耗/热环境数据集中教学建筑单独区分####
+#行为拆分
+data.hznu.use.room.day$buildingCode<-substr(data.hznu.use.room.day$roomCode,1,10)
+data.hznu.teaching.use<-data.hznu.use.room.day[buildingCode %in% 
+                                  data.mapping.buildingType[type=="teaching"]$buildingCode]
+#能耗拆分
+data.hznu.energy.room.day$buildingCode<-substr(data.hznu.energy.room.day$roomCode,1,10)
+data.hznu.teaching.energy<-data.hznu.energy.room.day[buildingCode %in% 
+                                                 data.mapping.buildingType[type=="teaching"]$buildingCode]
+
+
+####简单统计####
+#待需要时
