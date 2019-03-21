ed21c2e6cf4ab4862caf67a57f4b41f6a3ac2bd4
Li Xinyue <lxy_2008focus@126.com>
2019-03-21 10:17:34 +0800

    增加了对全年的EUI统计，对于高校建筑对不同校园类型进行了分类

--- ST_Complex.R
+++ ST_Complex.R
@@ -47,6 +47,33 @@ boxplot(data=data.raw.final[DataSource=="Campus"],eui~month+year,outline=TRUE)
 ggplot(data=data.raw.final[DataSource=="Campus"&eui!=0&eui<3&typeCode%in% c("D")],
          aes(x=month,y=eui,fill=year))+geom_boxplot()+theme_bw()
 
+####对于不同高校分类统计####
+data.hashmap.schoolType<-read.xlsx(file = "schoolTypeMapping.xlsx",sheetIndex = 1)
+data.raw.final$SchoolName<-matrix(data = unlist(strsplit(data.raw.final$BuildingCode,split = "_")),ncol = 3,byrow = TRUE)[,1]
+data.raw.final<-merge(x=data.raw.final,y=data.hashmap.schoolType[,c("SchoolCode","SchoolType")],all.x = TRUE,by.x = "SchoolName",by.y = "SchoolCode")
+ggplot(data = data.raw.final[DataSource=="Campus"&eui!=0],aes(x=month,y=eui,color=SchoolType))+
+  ylim(0,10)+geom_boxplot()+facet_wrap(~typeCode,nrow = 3)
+
+####全年统计####
+data.raw.final$labelBuildingYear<-paste(data.raw.final$BuildingCode,data.raw.final$year,sep = "_")
+data.building.selection<-data.raw.final[eui>0,.(count=length(BuildingCode)),by=labelBuildingYear]
+data.raw.pickFullYear<-data.raw.final[labelBuildingYear %in% unique(data.building.selection[count==12]$labelBuildingYear)]
+data.all.annualSum<-data.raw.pickFullYear[eui!=0,.(BuildingCode=BuildingCode[1],
+                                                   DataSource=DataSource[1],
+                                                   typeCode=typeCode[1],
+                                                   year=year[1],
+                                                   annualEnergy=sum(value,na.rm = TRUE),
+                                                   area=area[1]),by=labelBuildingYear]
+data.all.annualSum$annualEUI<-data.all.annualSum$annualEnergy/data.all.annualSum$area
+boxplot(data=data.all.annualSum[DataSource=="CPLX"],annualEUI~typeCode,outline=FALSE,xlab="Type Code",ylab="Annual EUI (kWh/ m^2)")
+
+####建筑情况统计####
+data.building.info<-data.raw.final[,.(count=length(unique(BuildingCode)),
+                                      meanArea=mean(area,na.rm = TRUE),
+                                      maxArea=max(area,na.rm = TRUE),
+                                      minArea=min(area,na.rm = TRUE)
+                                                   ),by=typeCode]
+
 ####箱形图统计####
 nn<-boxplot(data = data.raw.final[eui!=0&typeCode=="D"],eui~month+typeCode,outline=FALSE)
 stat.raw<-data.table(nn$stats)
@@ -66,3 +93,24 @@ for(i in LETTERS[c(1:20)]){
   boxplot(data = data.raw.final[typeCode==i & eui!=0],eui~month,outline=FALSE,xlab="Month",ylab="EUI (kWh/ m^2)")
   title(paste("Type:",i," n=",nrow(data.raw.final[typeCode==i & eui!=0])))
 }
+ggplot(data=data.raw.final[typeCode==""])
+
+####简单统计量####
+#获取全年箱形图统计参数
+nn<-boxplot(data = data.raw.final[eui!=0],eui~typeCode,outline=FALSE)
+stat.all.box.raw<-data.table(nn$stats)
+names(stat.all.box.raw)<-as.character(nn$names)
+stat.all.box.raw$name<-c("lowerWhisker","lowerHinge","median","upperHinge","upperWhisker")
+stat.all.box.final<-melt(data=stat.all.box.raw,id.vars = "name")
+
+#统计全年方差均值等
+stat.all.simple<-data.table(typeCode=unique(data.raw.final$typeCode))
+stat.all.simple$std<--1
+stat.all.simple$mean<--1
+for(i in unique(stat.all.simple$typeCode)){
+  stat.all.simple[typeCode==i]$std<-sd(data.raw.final[typeCode==i&eui>0]$eui)
+  stat.all.simple[typeCode==i]$mean<-mean(data.raw.final[typeCode==i&eui>0]$eui)
+}
+# eui<stat.all.box.final[name=="upperWhisker"&variable==i]$value &
+#未清洗
+write.xlsx(x=stat.all.simple,file = "RAW_SimpleStat.xlsx")
