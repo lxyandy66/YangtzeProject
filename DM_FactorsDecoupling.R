2d5b737375ee0293938f4648548c6076246238d4
Li Xinyue <lxy_2008focus@126.com>
2019-05-26 23:03:57 +0800

    尝试进行解耦，经典决策树效果不理想

--- DM_FactorsDecoupling.R
+++ DM_FactorsDecoupling.R
@@ -1,117 +1,65 @@
 ####影响因素解耦部分####
 
-#接list.hznu.room.use
-#已统一全年行为模式名称
+# data.hznu.use.final 行为已聚类数据
+# data.hznu.teaching.energy.std 能耗已聚类数据
+# data.hznu.teaching.thermo.final 热环境已聚类数据
 
-data.hznu.use.final<-as.data.table(list.hznu.room.use[[1]])
-for(i in c(2:6)){
-  data.hznu.use.final<-rbind(data.hznu.use.final,list.hznu.room.use[[i]])
+####以能耗模式为基准合并####
+data.hznu.teaching.decoupling<-merge(
+  x=data.hznu.teaching.energy.std[,c("labelRoomDay","roomCode","date","acCount","sumUiElec",sprintf("euiH%d",8:22),"energyClusterName")],
+  y=data.hznu.teaching.thermo.final[,c("labelRoomDay","runtime","modiSeason","finalState","clusterName","thermoPattern")],
+  all.x = TRUE,by.x = "labelRoomDay",by.y = "labelRoomDay")
+data.hznu.teaching.decoupling<-
+  data.hznu.teaching.decoupling[complete.cases(data.hznu.teaching.decoupling[,c("clusterName","thermoPattern")])]
+
+data.hznu.teaching.decoupling$areaScale<-apply(data.hznu.teaching.decoupling[,"acCount"],MARGIN = 1, FUN = getAreaLevel)
+
+
+####训练集/测试集划分####
+
+for(i in unique(data.hznu.teaching.decoupling[finalState=="cooling"]$clusterName)){
+  #分块处理
+  data.hznu.teaching.decoupling.selected<-data.hznu.teaching.decoupling[finalState=="cooling" & clusterName==i]
+  data.hznu.teaching.decoupling.selected$energyClusterName<-as.factor(data.hznu.teaching.decoupling.selected$energyClusterName)
+  data.hznu.teaching.decoupling.selected$thermoPattern<-as.factor(data.hznu.teaching.decoupling.selected$thermoPattern)
+  data.hznu.teaching.decoupling.selected$clusterName<-as.factor(data.hznu.teaching.decoupling.selected$clusterName)
+  data.hznu.teaching.decoupling.selected$areaScale<-as.factor(data.hznu.teaching.decoupling.selected$areaScale)
+  data.hznu.teaching.decoupling.selected$modiSeason<-as.factor(data.hznu.teaching.decoupling.selected$modiSeason)
+  data.hznu.teaching.decoupling.selected$runtime<-as.factor(data.hznu.teaching.decoupling.selected$runtime)
+  
+  ####训练集/测试集划分####
+  set.seed(711)
+  sub<-sample(1:nrow(data.hznu.teaching.decoupling.selected),round(nrow(data.hznu.teaching.decoupling.selected))*8/10)
+  data.hznu.teaching.decoupling.training<-data.hznu.teaching.decoupling.selected[sub]
+  data.hznu.teaching.decoupling.test<-data.hznu.teaching.decoupling.selected[-sub]
+  
+  
+  tree.both<-rpart(energyClusterName~clusterName+thermoPattern+areaScale+modiSeason+runtime,
+                   data=data.hznu.teaching.decoupling.training)#rpart,即经典决策树，必须都为factor或定性,连char都不行...
+  # tree.both$cptable
+  # tree.both<-prune(tree.both, cp= tree.both$cptable[which.min(tree.both$cptable[,"xerror"]),"CP"])#最优剪枝
+  rpartTrue2<-as.party(tree.both)#class(rpartTrue2)------[1]"constparty" "party" 
+  
+  plot(rpartTrue2,main=i)
+  
+  ####以测试集验证####
+  rtree.predict<-predict(rpartTrue2,data.hznu.teaching.decoupling.test)
+  confusionMatrix(table(rtree.predict,data.hznu.teaching.decoupling.test$energyClusterName))
+  # capture.output(
+  #   c(confusionMatrix(table(rtree.predict,data.hznu.teaching.decoupling.test$energyClusterName)),asRules(tree.both)),
+  #   file =paste("HZNU_Decoupling_Cooling_决策树评估_bestCP.txt")
+  # )
 }
 
 
-####全年基本情况统计####
-ggplot(data=data.hznu.use.final,aes(x=modiSeason,y=runtime))+geom_boxplot()
-boxplot(data = data.hznu.use.final,runtime~
-          paste(data.hznu.use.final$modiSeason,data.hznu.use.final$finalState))
-#按空调模式统计数据
-stat.use.room.cluster.byMode<-data.hznu.use.final[,.(
-  count=length(labelRoomDay),
-  finalState=unique(finalState),
-  clusterName=unique(clusterName),
-  runtime=mean(runtime,na.rm = TRUE),
-  h8=mean(h8,na.rm = TRUE),
-  h9=mean(h9,na.rm = TRUE),
-  h10=mean(h10,na.rm = TRUE),
-  h11=mean(h11,na.rm = TRUE),
-  h12=mean(h12,na.rm = TRUE),
-  h13=mean(h13,na.rm = TRUE),
-  h14=mean(h14,na.rm = TRUE),
-  h15=mean(h15,na.rm = TRUE),
-  h16=mean(h16,na.rm = TRUE),
-  h17=mean(h17,na.rm = TRUE),
-  h18=mean(h18,na.rm = TRUE),
-  h19=mean(h19,na.rm = TRUE),
-  h20=mean(h20,na.rm = TRUE),
-  h21=mean(h21,na.rm = TRUE),
-  h22=mean(h22,na.rm = TRUE)
-),by=paste(clusterName,finalState,sep = "_")]
-plot.use.room.cluster.byMode<-melt(data = stat.use.room.cluster.byMode[,c("paste","finalState","clusterName",sprintf("h%d",8:22))],
-                                   id = c("paste","finalState","clusterName"))
-ggplot(data = plot.use.room.cluster.byMode[finalState=="cooling"],
-       aes(x=variable,y=value,group=paste,color=paste,shape=paste))+geom_point()+geom_line()
-ggplot(data = data.hznu.use.final[finalState=="cooling"],aes(x=clusterName,y=runtime,color=clusterName))+geom_boxplot()
-
-stat.use.clusterDescribe<-data.hznu.use.final[,.(
-  count=length(labelRoomDay),
-  runtime=mean(runtime,na.rm = TRUE),
-  h8=mean(h8,na.rm = TRUE),
-  h9=mean(h9,na.rm = TRUE),
-  h10=mean(h10,na.rm = TRUE),
-  h11=mean(h11,na.rm = TRUE),
-  h12=mean(h12,na.rm = TRUE),
-  h13=mean(h13,na.rm = TRUE),
-  h14=mean(h14,na.rm = TRUE),
-  h15=mean(h15,na.rm = TRUE),
-  h16=mean(h16,na.rm = TRUE),
-  h17=mean(h17,na.rm = TRUE),
-  h18=mean(h18,na.rm = TRUE),
-  h19=mean(h19,na.rm = TRUE),
-  h20=mean(h20,na.rm = TRUE),
-  h21=mean(h21,na.rm = TRUE),
-  h22=mean(h22,na.rm = TRUE)
-),by=clusterName]
-stat.use.clusterDescribe.plot<-melt(stat.use.clusterDescribe[,-c("count","runtime")],id.vars = c("clusterName"))
-ggplot(data = stat.use.clusterDescribe.plot,aes(x=variable,y=value,group=clusterName,color=clusterName,shape=clusterName))+geom_point()+geom_line()
 
 
-#转换为因子的预处理
-modeSelect<-"cooling"
-data.hznu.use.final.modePickup<-data.hznu.use.final[finalState==modeSelect]#&modiSeason %in% c("Summer_warm","Transition" )]
-# > class(data.hznu.use.final.modePickup[,"h8"])
-# [1] "data.table" "data.frame"
-# > class(data.hznu.use.final.modePickup$h8)
-# [1] "numeric"
-#不能按列批量直接取
-
-
-data.hznu.use.final.modePickup[,c(paste("h",c(8:22),sep = ""))]<-
-  data.table(sapply(data.hznu.use.final.modePickup[,c(paste("h",c(8:22),sep = ""))],as.factor),stringsAsFactors = TRUE)
-data.hznu.use.final.modePickup$modiSeason<-as.factor(data.hznu.use.final.modePickup$modiSeason)
-#注意data.table中stringsAsFactors参数，此处sapply先转成matrix，因此转换成data.table时该参数会默认为false，即为字符串型
-
-####统计各模式运行时间####
-stat.use.runtime<-boxplot(data = data.hznu.use.final,runtime~data.hznu.use.final$clusterName)
-data.hznu.use.final.modePickup$runtimeClass<-as.factor(sapply(data.hznu.use.final.modePickup$runtime,getRuntimeClass))
-# data.hznu.use.final.modePickup[clusterName=="halfDaytime"]$clusterName<-"daytime"
-
-####训练集/测试集划分####
-set.seed(711)
-sub<-sample(1:nrow(data.hznu.use.final.modePickup),round(nrow(data.hznu.use.final.modePickup))*3/4)
-data.hznu.use.tree.training<-data.hznu.use.final.modePickup[sub]
-data.hznu.use.tree.test<-data.hznu.use.final.modePickup[-sub]
-
-tree.both<-rpart(clusterName~modiSeason+runtimeClass+h8+h9+h10+h11+h12
-                 +h13+h14+h15+h16+h17+h18
-                 +h19+h20+h21+h22,data=data.hznu.use.tree.training)#rpart,即经典决策树，必须都为factor或定性,连char都不行...
-tree.both$cptable
-tree.both<-prune(tree.both, cp= tree.both$cptable[which.min(tree.both$cptable[,"xerror"]),"CP"])#最优剪枝
-rpartTrue2<-as.party(tree.both)#class(rpartTrue2)------[1]"constparty" "party" 
-plot(rpartTrue2)
-
-####以测试集验证####
-rtree.predict<-predict(rpartTrue2,data.hznu.use.tree.test)
-confusionMatrix(table(rtree.predict,data.hznu.use.tree.test$clusterName))
-
-capture.output(c(confusionMatrix(table(rtree.predict,data.hznu.use.tree.test$clusterName)),asRules(tree.both)),
-               file =paste("HZNU_行为_制冷_决策树评估_bestCP.txt"))
-
-
-getRuntimeClass<-function(time){
-  if(time<=3)
-    return("<=3")
-  if(time<=6)
-    return("(3,6]")
-  if(time<=9)
-    return("(6,9]")
-  return(">9")
+####获取不同空调数对应的面积等级####
+getAreaLevel<-function(acCount){
+  if(acCount<=2)
+    return("Small")
+  if(acCount<=8)
+    return("Mid")
+  if(acCount<=16)
+    return("Large")
 }
