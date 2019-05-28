19a07a8e7bb9ed1a40a84e109792009ee80607a6
Li Xinyue <lxy_2008focus@126.com>
2019-05-28 23:40:04 +0800

    对两种变量组合的行为-能耗模式进行解耦，尝试了5种算法

--- DM_FactorsDecoupling.R
+++ DM_FactorsDecoupling.R
@@ -35,8 +35,10 @@ data.hznu.teaching.decoupling$areaScale<-apply(data.hznu.teaching.decoupling[,"a
   data.hznu.teaching.decoupling.test<-data.hznu.teaching.decoupling.selected[-sub]
   
   #CART决策树算法
-  tree.both<-rpart(energyClusterName~clusterName+areaScale+modiSeason,method = "class",parms=list(split="gini"),
+  tree.both<-rpart(energyClusterName~clusterName+areaScale+modiSeason+runtime,method = "class",parms=list(split="gini"),
                    data=data.hznu.teaching.decoupling.training)#rpart,即经典决策树，必须都为factor或定性,连char都不行...
+  rpartTrue2<-as.party(tree.both)#class(rpartTrue2)------[1]"constparty" "party" 
+  plot(rpartTrue2)
   # tree.both$cptable
   # tree.both<-prune(tree.both, cp= tree.both$cptable[which.min(tree.both$cptable[,"xerror"]),"CP"])#最优剪枝
  
@@ -44,31 +46,31 @@ data.hznu.teaching.decoupling$areaScale<-apply(data.hznu.teaching.decoupling[,"a
   # tree.both<-C5.0(energyClusterName~thermoPattern+clusterName+areaScale+modiSeason,data=data.hznu.teaching.decoupling.training)
   # 
   #CTree
-  tree.both<-ctree(energyClusterName~clusterName+areaScale+modiSeason,
+  tree.both<-ctree(energyClusterName~clusterName+areaScale+modiSeason+runtime,
                    data=data.hznu.teaching.decoupling.training)
   rpartTrue2<-tree.both
   
   # #随机森林
-  fit.forest<-cforest(energyClusterName~clusterName+areaScale+modiSeason+runtime,
-                           data=data.hznu.teaching.decoupling.training)
+  fit.forest<-randomForest(energyClusterName~clusterName+areaScale+modiSeason+runtime,
+                           data=data.hznu.teaching.decoupling.training,ntree=1000)
   rpartTrue2<-fit.forest
   
   ####AdaBoost####
   #这里有问题
   tree.both<-boosting(energyClusterName~clusterName+areaScale+modiSeason+runtime,
-                      data=data.hznu.teaching.decoupling.training)
+                      data=data.hznu.teaching.decoupling.training,mfinal = 500)
   rpartTrue2<-tree.both
+  #检查误差演变
+  b<-errorevol(rpartTrue2,data.hznu.teaching.decoupling.training)                       #计算全体的误差演变
+  plot(b$error,type="l",main="AdaBoost error vs number of trees")
   
   ####SVM####
-  regm<-ksvm(energyClusterName~clusterName+areaScale,
+  regm<-ksvm(energyClusterName~clusterName+areaScale+modiSeason+runtime,
              data=data.hznu.teaching.decoupling.training,
-             kernel="vanilladot",type="C-bsvc",C=30,cross=10,prob.model=TRUE)
+             kernel="rbfdot",type="C-bsvc",C=10,cross=10)
   rpartTrue2<-regm
   
-  
-  rpartTrue2<-as.party(tree.both)#class(rpartTrue2)------[1]"constparty" "party" 
-  plot(rpartTrue2,main=i)
-  
+
   
   ####以训练集验证####
   rtree.training<-predict(rpartTrue2,data.hznu.teaching.decoupling.training)
@@ -76,6 +78,7 @@ data.hznu.teaching.decoupling$areaScale<-apply(data.hznu.teaching.decoupling[,"a
   ####以测试集验证####
   rtree.predict<-predict(rpartTrue2,data.hznu.teaching.decoupling.test)
   confusionMatrix(table(rtree.predict,data.hznu.teaching.decoupling.test$energyClusterName))
+  rm(rpartTrue2)
   
 # }
 
