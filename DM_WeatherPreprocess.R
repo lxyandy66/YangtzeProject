f2eed6e983b66284f32da7818b18eac75683c1bf
Li Xinyue <lxy_2008focus@126.com>
2019-06-02 14:56:19 +0800

    代码保存中文乱码修复

--- DM_WeatherPreprocess.R
+++ DM_WeatherPreprocess.R
@@ -1,23 +1,23 @@
-####½ÓÆøÏóÕ¾Êý¾Ý####
-fileName<-c("ÈÕÆÚ2016-10-27???2016-10-31.xls",
-            "ÈÕÆÚ2016-11-01???2016-11-04.xls",
-            "ÈÕÆÚ2016-11-04???2016-11-16.xls",
-            "ÈÕÆÚ2016-11-16???2016-11-30.xls",
-            "ÈÕÆÚ2016-11-30.xls",
-            "ÈÕÆÚ2017-04-01???2017-04-05.xls",
-            "ÈÕÆÚ2017-04-05???2017-04-30.xls",
-            "ÈÕÆÚ2017-05-01???2017-05-05.xls",
-            "ÈÕÆÚ2017-05-05???2017-05-15.xls",
-            "ÈÕÆÚ2017-05-15???2017-05-31.xls",
-            "ÈÕÆÚ2017-06-01???2017-06-07.xls",
-            "ÈÕÆÚ2017-06-07???2017-06-28.xls",
-            "ÈÕÆÚ2017-09-01???2017-09-11.xls",
-            "ÈÕÆÚ2017-09-11???2017-09-30.xls",
-            "ÈÕÆÚ2017-10-01???2017-10-18.xls")
+####æŽ¥æ°”è±¡ç«™æ•°æ®####
+fileName<-c("æ—¥æœŸ2016-10-27???2016-10-31.xls",
+            "æ—¥æœŸ2016-11-01???2016-11-04.xls",
+            "æ—¥æœŸ2016-11-04???2016-11-16.xls",
+            "æ—¥æœŸ2016-11-16???2016-11-30.xls",
+            "æ—¥æœŸ2016-11-30.xls",
+            "æ—¥æœŸ2017-04-01???2017-04-05.xls",
+            "æ—¥æœŸ2017-04-05???2017-04-30.xls",
+            "æ—¥æœŸ2017-05-01???2017-05-05.xls",
+            "æ—¥æœŸ2017-05-05???2017-05-15.xls",
+            "æ—¥æœŸ2017-05-15???2017-05-31.xls",
+            "æ—¥æœŸ2017-06-01???2017-06-07.xls",
+            "æ—¥æœŸ2017-06-07???2017-06-28.xls",
+            "æ—¥æœŸ2017-09-01???2017-09-11.xls",
+            "æ—¥æœŸ2017-09-11???2017-09-30.xls",
+            "æ—¥æœŸ2017-10-01???2017-10-18.xls")
 data.weather.raw<-read.xlsx2(sheetIndex = 1,colClasses=c("Date","character"),
                              file = i)
 data.weather.raw<-data.weather.raw[0,]
-for(i in paste("Êý¾Ý-ÆøÏóÕ¾Êý¾Ý\\",fileName,sep="")){
+for(i in paste("æ•°æ®-æ°”è±¡ç«™æ•°æ®\\",fileName,sep="")){
   temp.weather.raw<-data.table(
     read.xlsx2(sheetIndex = 1,colClasses=c("Date","character"),file = i))
   tryCatch({
@@ -26,15 +26,15 @@ for(i in paste("Êý¾Ý-ÆøÏóÕ¾Êý¾Ý\\",fileName,sep="")){
     data.weather.raw<-rbind(data.weather.raw,temp.weather.raw[naSum<5,1:23])
   },error=function(e){cat("file = ",i,e)} )
 }
-#ºÏ²¢ºóÈÕÆÚÓëÊ±¼ä´¦ÀíÕý³£
+#åˆå¹¶åŽæ—¥æœŸä¸Žæ—¶é—´å¤„ç†æ­£å¸¸
 
-####ÖÐÎÄ±äÁ¿Ìæ»»####
+####ä¸­æ–‡å˜é‡æ›¿æ¢####
 names(data.weather.raw)<-c( "date","time","temp1","temp2","temp3","temp4","temp5",
                             "outTemp","rhOut","dewTemp","pressure","lat","windDir","WindSpeed",
                             "meanWind2Min","meanWind10Min","rainSum","light","curRad1","sumRad1","curRad2",  
                             "sumRad2","sunTime")
-#³ýÈ¥Ò»Ð©±äÁ¿
+#é™¤åŽ»ä¸€äº›å˜é‡
 data.weather.raw[,c("temp1","temp2","temp3","temp4","temp5")]<-NULL
 data.weather.station.raw<-data.weather.raw
-#ÔÝ´æ
-save(data.weather.station.raw,file="HZ_ÆøÏóÕ¾Êý¾Ý.rdata")
+#æš‚å­˜
+save(data.weather.station.raw,file="HZ_æ°”è±¡ç«™æ•°æ®.rdata")
